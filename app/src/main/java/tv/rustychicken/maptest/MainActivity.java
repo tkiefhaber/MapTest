e1df6bf9c7b5e25dd8222cb8fe8990b206572d2b
Tom Kiefhaber <tom.kiefhaber@livingsocial.com>
2016-02-09 14:48:55 -0500

    initial commit

---
+++ app/src/main/java/tv/rustychicken/maptest/MainActivity.java
@@ -0,0 +1,34 @@
+package tv.rustychicken.maptest;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    SupportMapFragment mapFragment;
+    GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng hydePark = new LatLng(39.139964, -84.4452855);
+        map.addMarker(new MarkerOptions().position(hydePark).title("Hyde Park Square"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(hydePark));
+    }
+}
